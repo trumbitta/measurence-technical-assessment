e75b97ffe1f90e069cf89872c36ee751ee410626
William Ghelfi <trumbitta@gmail.com>
2017-08-20 16:20:33 +0200

    Add proper types

--- array-flatten/src/flatten/flatten.ts
+++ array-flatten/src/flatten/flatten.ts
@@ -1,9 +1,9 @@
-export function flattenArray(toFlatten) {
+export function flattenArray(toFlatten: any[]): number[] {
   return toFlatten.reduce(
     (arrayAccumulator, currentValue) => {
       const toConcat = Array.isArray(currentValue) ? this.flattenArray(currentValue) : currentValue;
       return arrayAccumulator.concat(toConcat);
     },
     []
-  );
+  ) as number[];
 }
--- array-flatten/src/index.ts
+++ array-flatten/src/index.ts
@@ -1,7 +1,9 @@
 import { flattenArray } from './flatten/flatten';
 
-const arrayNested = [[1, 2, [3, 4, [5, 6]]], 7, [8]];
-const arrayFlat = flattenArray(arrayNested);
+// I think it's overkill to implement something like https://stackoverflow.com/a/42621557/1030207 for this test,
+// so the function won't be type-checking the input array besides being an array.
+const arrayNested: any[] = [[1, 2, [3, 4, [5, 6]]], 7, [8]];
+const arrayFlat: number[] = flattenArray(arrayNested);
 
 /* tslint:disable:no-console */
 console.log('Nested:\n\t', arrayNested);
